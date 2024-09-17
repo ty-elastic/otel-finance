4b6b4ee262cf8cb44fbdc40dec60f57cdd8552f8
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-09-17 08:49:34 -0500

    more

--- src/decider/app.py
+++ src/decider/app.py
@@ -4,10 +4,11 @@ import time
 import random
 import os
 import uuid
+import math
 
 import requests
-from opentelemetry import trace, baggage, context
-from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry import trace, baggage
+from opentelemetry.metrics import get_meter
 from opentelemetry.processor.baggage import BaggageSpanProcessor, ALLOW_ALL_BAGGAGE_KEYS
 
 app = Flask(__name__)
@@ -16,70 +17,116 @@ app.logger.setLevel(logging.INFO)
 tracer_provider = trace.get_tracer_provider()
 tracer_provider.add_span_processor(BaggageSpanProcessor(ALLOW_ALL_BAGGAGE_KEYS))
 
-tracer = trace.get_tracer("decider.tracer")
+tracer = trace.get_tracer("decider")
 
+meter = get_meter("decider")
+trade_fee_dollars = meter.create_counter("trade-fee-dollars")
+trade_volume_shares = meter.create_counter("trade-volume-shares")
+trade_volume_dollars = meter.create_counter("trade-volume-dollars")
+
+def sim_errors(*, strategy, symbol):
+    
+    latency_model = 0
+    error_model = False
+    error_db = False
+
+    if strategy == 'conservative':
+        latency_model = random.randint(50, 60) / 100.0
+        if symbol == 'BAD':
+            error_db = True
+    else:
+        if symbol == 'ERR':
+            error_model = True
+
+    return latency_model, error_model, error_db
+
+def sim_market_data(*, strategy, symbol):
+    pr_volume = random.randint(-100, 100)
+    return pr_volume
+
+def sim_decide(*, strategy, symbol, pr_volume):
+    
+    action = 'hold'
+    rand = random.randint(0, 2)
+    if rand == 1:
+        action = 'buy'
+    elif rand == 1:
+        action = 'sell'
+        
+    shares = random.randint(1, 10 if strategy == 'conservative' else 100 if strategy == 'balanced' else 1000)
+    share_price = random.randint(1, 500)
+    
+    return action, shares, share_price
+    
 @app.post('/decide')
 def decide():
+    current_span = trace.get_current_span()
     
     trade_id = str(uuid.uuid4())
-    current_span = trace.get_current_span()
-    current_span.set_attribute("trade_id", trade_id)
+    #current_span.set_attribute("trade_id", trade_id)
+    baggage.set_baggage("trade_id", trade_id)
     
-    context.attach(baggage.set_baggage("trade_id", trade_id))
-    
-    bags = baggage.get_all()
-    print(bags)
-    
-    strategy = request.args.get('strategy', default='aggressive', type=str)
-    pr_volume = request.args.get('pr_volume', default=0, type=int)
+    strategy = request.args.get('strategy', default='balanced', type=str)
     symbol = request.args.get('symbol', default='ESTC', type=str)
+
+    #current_span.set_attribute("symbol", symbol)
+    baggage.set_baggage("symbol", symbol)
+
+    latency_model, error_model, error_db = sim_errors(strategy=strategy, symbol=symbol )
+    current_span.set_attribute("latency_model", latency_model)
+    current_span.set_attribute("error_model", error_model)
+    current_span.set_attribute("error_db", error_db)
+     
+    action, shares, share_price = decide_model(strategy=strategy, symbol=symbol, error=error_model, latency=latency_model)
+
+    response = {}
+    response['id'] = trade_id
+    response['symbol']= symbol
+        
+    if error_db is True:
+        symbol = None
+        print('error DB!')
+    trade_response = requests.post(f"http://{os.environ['TRADER_HOST']}:9000/trade", params={'id': trade_id, 'symbol': symbol, 'shares': shares, 'share_price': share_price, 'action': action})
+    trade_response.raise_for_status()
+    trade_response_json = trade_response.json()
+
+    response['shares']= shares
+    response['share_price']= share_price
+    response['action']= action
     
-    model_error = request.args.get('model_error', default='false', type=str)
-    model_latency = request.args.get('model_latency', default=0, type=int)
-    
-    sql_error = request.args.get('sql_error', default='false', type=str)
-
-    app.logger.info("deciding to trade")
-    
-    action, shares, share_cost = decide_model(strategy=strategy, pr_volume=pr_volume, symbol=symbol, error=model_error, latency=model_latency)
-
-    response = requests.post(f"http://{os.environ['TRADER_HOST']}:9000/trade", params={'id': trade_id, 'symbol': symbol, 'shares': shares, 'share_cost': share_cost, 'action': action, 'error': sql_error})
-    response_json = response.json()
-
-    response_json['id'] = trade_id
-    response_json['strategy']= strategy
-    response_json['symbol']= symbol
-    response_json['shares']= shares
-    response_json['action']= action
-    response_json['share_cost']= share_cost
-
-    return response_json
+    return response
 
 @tracer.start_as_current_span("decide_model")
-def decide_model(*, strategy, pr_volume, symbol, error=False, latency=0):
+def decide_model(*, strategy, symbol, error=False, latency=0.0):
+    
+    app.logger.info("deciding to trade")
+    
+    pr_volume = sim_market_data(strategy=strategy, symbol=symbol)
     
     current_span = trace.get_current_span()
-    current_span.set_attribute("in.strategy", strategy)
     current_span.set_attribute("in.pr_volume", pr_volume)
-    current_span.set_attribute("in.symbol", symbol)
+    current_span.set_attribute("in.strategy", strategy)
+    
+    if error:
+        raise Exception("model failed") 
     
     ## MODEL WOULD BE CALLED HERE
-    
-    if error == 'true':
-        raise Exception("model failed") 
+    action, shares, share_price = sim_decide(strategy=strategy, symbol=symbol, pr_volume=pr_volume)
     if latency > 0:
         time.sleep(latency)
-        
-    action = 'buy' if random.randint(0, 1) == 0 else 'sell'
-    shares = random.randint(1, 10 if strategy == 'conservative' else 100 if strategy == 'balanced' else 1000)
-    share_cost = random.randint(1, 500)
-  
-    current_span.set_attribute("out.share_cost", share_cost)
-    current_span.set_attribute("out.action", action)
-    current_span.set_attribute("out.shares", shares)
-    current_span.set_attribute("out.value", shares * share_cost)
 
-    return action, shares, share_cost
+    current_span.set_attribute("out.shares", shares)
+    current_span.set_attribute("out.share_price", share_price)
+    current_span.set_attribute("out.action", action)
+    if action == 'buy' or action == 'sell':
+        current_span.set_attribute("out.value", shares * share_price)
+        trade_fee_dollars.add(math.ceil(share_price * shares * .10))
+        trade_volume_shares.add(shares)
+        trade_volume_dollars.add(math.ceil(share_price * shares))
+    else:
+        current_span.set_attribute("out.value", 0)
+
+    return action, shares, share_price
     
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=9001)
--- src/trader/app.py
+++ src/trader/app.py
@@ -5,9 +5,7 @@ import math
 
 import psycopg
 
-from opentelemetry import trace, baggage
-from opentelemetry.metrics import get_meter
-from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry import trace
 from opentelemetry.processor.baggage import BaggageSpanProcessor, ALLOW_ALL_BAGGAGE_KEYS
 
 app = Flask(__name__)
@@ -16,60 +14,39 @@ app.logger.setLevel(logging.INFO)
 tracer_provider = trace.get_tracer_provider()
 tracer_provider.add_span_processor(BaggageSpanProcessor(ALLOW_ALL_BAGGAGE_KEYS))
 
-meter = get_meter("metric-trader")
-trade_fee_dollars = meter.create_counter("trade-fee-dollars")
-trade_volume_shares = meter.create_counter("trade-volume-shares")
-trade_volume_dollars = meter.create_counter("trade-volume-dollars")
-
 def init_db():
     try:
         with psycopg.connect(f"host={os.environ['POSTGRES_HOST']} port=5432 dbname=trades user={os.environ['POSTGRES_USER']} password={os.environ['POSTGRES_PASSWORD']}") as conn:
             with conn.cursor() as cur:
                 cur.execute(
-                    "CREATE TABLE trades (id VARCHAR(100) PRIMARY KEY, symbol VARCHAR(10), shares float NOT NULL, action VARCHAR(10))")
+                    "CREATE TABLE trades (id VARCHAR(100) PRIMARY KEY, symbol VARCHAR(10) NOT NULL, shares float NOT NULL, share_price float NOT NULL, action VARCHAR(10) NOT NULL)")
                 conn.commit()
+        app.logger.info("created table")
     except Exception as inst:
-        app.logger.warning("unable to recreate table")
+        app.logger.warning("unable to create table")
         pass
+init_db()
 
 @app.post('/trade')
 def trade():
 
     trade_id = request.args.get('id')
-    symbol = request.args.get('symbol', default=None, type=str)
+    symbol = request.args.get('symbol', default=None, type=None)
     shares = request.args.get('shares', default=None, type=float)
-    share_cost = request.args.get('share_cost', default=0, type=float)
+    share_price = request.args.get('share_price', default=0, type=float)
     action = request.args.get('action', default=None, type=str)
-   
-    error = request.args.get('error', default='false', type=str)
-        
-    current_span = trace.get_current_span()
-    current_span.set_attribute("in.symbol", symbol)
-    current_span.set_attribute("in.shares", shares)
-    current_span.set_attribute("in.share_cost", share_cost)
-    current_span.set_attribute("in.action", action)
     
-    print(error)
-    if error == 'true':
-        shares = None
-     
     with psycopg.connect(f"host={os.environ['POSTGRES_HOST']} port=5432 dbname=trades user={os.environ['POSTGRES_USER']} password={os.environ['POSTGRES_PASSWORD']}") as conn:
         with conn.cursor() as cur:
             cur.execute(
-                "INSERT INTO trades (id, symbol, action, shares) VALUES (%s, %s, %s, %s)",
-                (trade_id, symbol, action, shares))
+                "INSERT INTO trades (id, symbol, action, shares, share_price) VALUES (%s, %s, %s, %s, %s)",
+                (trade_id, symbol, action, shares, share_price))
             conn.commit()
-            
-            trade_fee_dollars.add(math.ceil(share_cost * shares * .10))
-            trade_volume_shares.add(shares)
-            trade_volume_dollars.add(math.ceil(share_cost * shares))
-            
-            current_span.set_attribute("out.result", 'committed')
-            
+
             result = {'result': 'committed', 'id': trade_id}
             app.logger.info(result)
             return result
 
 if __name__ == '__main__':
-    init_db()
+    
     app.run(host="0.0.0.0", port=9000)
