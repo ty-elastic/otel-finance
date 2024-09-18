a540b6fbcf3ff9836ad03a0a9e02d5a9fc167b3a
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-09-17 21:04:52 -0500

    done

--- src/world/app.py
+++ src/world/app.py
@@ -1,7 +1,12 @@
+from flask import Flask, request
+import logging
 import requests
 import random
 import time
 import os
+
+app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
 
 def generate_trade(*, symbol, day_of_week):
     try:
@@ -12,6 +17,9 @@ def generate_trade(*, symbol, day_of_week):
 
 DAYS_OF_WEEK = ['M','Tu', 'W', 'Th', 'F']
 
+latency_upper_bound = 50
+error_rate = 10
+
 def generate_trades():
     symbols = ['MOT', 'MSI', 'GOGO', 'INTEQ', 'VID', 'ESTC', 'OD1', 'OD2']
     idx_of_week = 0
@@ -21,12 +29,33 @@ def generate_trades():
         trades_per_day = random.randint(0, 100)
         for i in range(trades_per_day):
             symbol = symbols[random.randint(0, len(symbols)-1)]
-            if random.randint(0, 100) > 90:
+            if random.randint(0, 100) > (100-error_rate):
                 symbol = 'ERR'
             print(f"trading for {symbol} on {DAYS_OF_WEEK[idx_of_week]}, count={i}")
             generate_trade(symbol=symbol, day_of_week=DAYS_OF_WEEK[idx_of_week])
-            latency = float(random.randint(1, 50)) / 1000.0
+            latency = float(random.randint(0, latency_upper_bound)) / 1000.0
             time.sleep(latency)
 
-if __name__ == '__main__':
-    generate_trades()
+@app.post('/tput/fast')
+def tput_fast():
+    global latency_upper_bound
+    latency_upper_bound = 1
+    return {'KERNEL': 'OK'}
+    
+@app.post('/tput/default')
+def tput_default():
+    global latency_upper_bound
+    latency_upper_bound = 50
+    return {'KERNEL': 'OK'}
+    
+@app.post('/err/high')
+def err_high():
+    global latency_upper_bound
+    error_rate = 75
+    return {'KERNEL': 'OK'}
+    
+@app.post('/err/default')
+def err_default():
+    global latency_upper_bound
+    error_rate = 10
+    return {'KERNEL': 'OK'}
--- src/world/test.sh
+++ src/world/test.sh
@@ -1,3 +1,3 @@
 export DECIDER_HOST=127.0.0.1
 
-python app.py
+flask run --host=0.0.0.0 -p 9002
