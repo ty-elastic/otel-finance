bb3de62155b28c5c3ca9377781bb00ff7d91384b
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-09-28 16:15:22 -0500

    ty

---
+++ src/lib/java/baggage-log-record-processor/build/spotless/spotlessJava/src/main/java/io/opentelemetry/contrib/baggage/logrecord/processor/BaggageLogRecordProcessorCustomizer.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.contrib.baggage.logrecord.processor;
+
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
+import io.opentelemetry.sdk.logs.SdkLoggerProviderBuilder;
+
+public class BaggageLogRecordProcessorCustomizer implements AutoConfigurationCustomizerProvider {
+  @Override
+  public void customize(AutoConfigurationCustomizer autoConfigurationCustomizer) {
+    autoConfigurationCustomizer.addLoggerProviderCustomizer(
+        (sdkLoggerProviderBuilder, config) -> {
+          addLogRecordProcessor(sdkLoggerProviderBuilder, config);
+          return sdkLoggerProviderBuilder;
+        });
+  }
+
+  private static void addLogRecordProcessor(
+      SdkLoggerProviderBuilder sdkLoggerProviderBuilder, ConfigProperties config) {
+    sdkLoggerProviderBuilder.addLogRecordProcessor(createProcessor());
+  }
+
+  static BaggageLogRecordProcessor createProcessor() {
+    return new BaggageLogRecordProcessor();
+  }
+}
