e1dc35fe267f7f3ed180d6fdc88f25e077a1bc43
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-09-29 22:00:45 -0500

    Ty

--- lib/build.sh
+++ lib/build.sh
@@ -1,16 +1,18 @@
 mkdir -p ../src/recorder-go/_lib/
 cp -r go/ ../src/recorder-go/_lib/
 
+mkdir -p ../src/recorder-java/_lib/
 cd java/baggage-log-record-processor
 docker build -t java-baggage-log-record-processor .
 id=$(docker create java-baggage-log-record-processor)
-docker cp $id:/usr/src/app/build/libs/opentelemetry-java-baggage-log-record-processor-1.0-all.jar - > ../../../src/recorder-java/_lib//opentelemetry-java-baggage-log-record-processor-all.jar
+docker cp $id:/usr/src/app/build/libs/opentelemetry-java-baggage-log-record-processor-1.0-all.jar - > ../../../src/recorder-java/_lib/opentelemetry-java-baggage-log-record-processor-all.jar
 docker rm -v $id
 cd ../..
 # ./gradlew build
 # mkdir -p ../../../src/recorder-java/_lib/
 # cp build/libs/opentelemetry-java-baggage-log-record-processor-1.0-all.jar ../../../src/recorder-java/_lib/opentelemetry-java-baggage-log-record-processor-all.jar
 
+mkdir -p ../src/recorder-java/_lib/
 wget -O ../src/recorder-java/_lib/elastic-otel-javaagent.jar https://repo1.maven.org/maven2/co/elastic/otel/elastic-otel-javaagent/1.0.0/elastic-otel-javaagent-1.0.0.jar
 
 mkdir -p ../src/trader/_lib/
