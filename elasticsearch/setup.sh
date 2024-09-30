c1d954f62f713b57dcc6f0e11c9e7a6bc6abf75f
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-09-30 05:58:47 -0500

    ty2

--- elasticsearch/setup.sh
+++ elasticsearch/setup.sh
@@ -1,11 +1,9 @@
-export ELASTICSEARCH_URL="https://${ES_USER}:${ES_PASS}@${ES_ADDRESS}:443"
-echo $ELASTICSEARCH_URL
-export KIBANA_URL="https://${ES_USER}:${ES_PASS}@${KB_ADDRESS}:9243"
-echo $KIBANA_URL
+export $(cat /root/.env | xargs)
+BASE64=$(echo -n "${ELASTICSEARCH_USER}:${ELASTICSEARCH_PASSWORD}" | base64)
 
 echo "create aliases"
-curl -XDELETE "$ELASTICSEARCH_URL/traces-apm-default/_alias/traces-apm-trader" -H "kbn-xsrf: reporting"
-curl -XPUT "$ELASTICSEARCH_URL/traces-apm-default/_alias/traces-apm-trader" -H "kbn-xsrf: reporting" -H "Content-Type: application/json" -d'
+curl -XDELETE --header "Authorization: Basic $BASE64" "$ELASTICSEARCH_URL/traces-apm-default/_alias/traces-apm-trader" -H "kbn-xsrf: reporting"
+curl -XPUT --header "Authorization: Basic $BASE64" "$ELASTICSEARCH_URL/traces-apm-default/_alias/traces-apm-trader" -H "kbn-xsrf: reporting" -H "Content-Type: application/json" -d'
 {
   "filter": {
     "term": {
@@ -15,5 +13,4 @@ curl -XPUT "$ELASTICSEARCH_URL/traces-apm-default/_alias/traces-apm-trader" -H "
 }'
 
 echo "load kibana resources"
-#curl -XPOST "$KIBANA_URL/api/saved_objects/_import?overwrite=true&compatibilityMode=true" -H "kbn-xsrf: true" --form file=@dashboards.ndjson
-curl -XPOST "$KIBANA_URL/api/saved_objects/_import?overwrite=true&compatibilityMode=true" -H "kbn-xsrf: true" --form file=@dashboards.ndjson
+curl -XPOST --header "Authorization: Basic $BASE64" "$KIBANA_URL/api/saved_objects/_import?overwrite=true&compatibilityMode=true" -H "kbn-xsrf: true" --form file=@dashboards.ndjson
