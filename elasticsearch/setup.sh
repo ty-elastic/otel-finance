af68471fd96c98c73bde2fd2e3e53836805dc6ac
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-09-30 06:02:45 -0500

    ty

--- elasticsearch/setup.sh
+++ elasticsearch/setup.sh
@@ -2,7 +2,6 @@ export $(cat /root/.env | xargs)
 BASE64=$(echo -n "${ELASTICSEARCH_USER}:${ELASTICSEARCH_PASSWORD}" | base64)
 
 echo "create aliases"
-curl -XDELETE --header "Authorization: Basic $BASE64" "$ELASTICSEARCH_URL/traces-apm-default/_alias/traces-apm-trader" -H "kbn-xsrf: reporting"
 curl -XPUT --header "Authorization: Basic $BASE64" "$ELASTICSEARCH_URL/traces-apm-default/_alias/traces-apm-trader" -H "kbn-xsrf: reporting" -H "Content-Type: application/json" -d'
 {
   "filter": {
