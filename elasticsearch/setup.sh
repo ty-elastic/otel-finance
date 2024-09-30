effabc5e3390c5ffd605b8994662717fe56e037f
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-09-30 05:51:59 -0500

    ty

--- elasticsearch/setup.sh
+++ elasticsearch/setup.sh
@@ -1,4 +1,7 @@
-ELASTICSEARCH_URL="https://${ES_USER}:${ES_PASS}@${ES_ENDPOINT}:443"
+export ELASTICSEARCH_URL="https://${ES_USER}:${ES_PASS}@${ES_ADDRESS}:443"
+echo $ELASTICSEARCH_URL
+export KIBANA_URL="https://${ES_USER}:${ES_PASS}@${KB_ADDRESS}:9243"
+echo $KIBANA_URL
 
 echo "create aliases"
 curl -XDELETE "$ELASTICSEARCH_URL/traces-apm-default/_alias/traces-apm-trader" -H "kbn-xsrf: reporting"
@@ -6,7 +9,11 @@ curl -XPUT "$ELASTICSEARCH_URL/traces-apm-default/_alias/traces-apm-trader" -H "
 {
   "filter": {
     "term": {
-                  "service.name": "trader"
-            }
+      "service.name": "trader"
+    }
   }
 }'
+
+echo "load kibana resources"
+#curl -XPOST "$KIBANA_URL/api/saved_objects/_import?overwrite=true&compatibilityMode=true" -H "kbn-xsrf: true" --form file=@dashboards.ndjson
+curl -XPOST "$KIBANA_URL/api/saved_objects/_import?overwrite=true&compatibilityMode=true" -H "kbn-xsrf: true" --form file=@dashboards.ndjson
