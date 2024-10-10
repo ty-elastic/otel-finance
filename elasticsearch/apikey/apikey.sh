e3fbccfbffdadfefb1daceb1e292ccc6c3f01266
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-10-10 10:10:45 -0500

    apikey

---
+++ elasticsearch/apikey/apikey.sh
@@ -0,0 +1,4 @@
+output=$(curl -X POST -s -u "${ELASTICSEARCH_USER}:${ELASTICSEARCH_PASSWORD}" $ELASTICSEARCH_URL/_security/api_key -H 'Content-Type: application/json' -d '{"name": "collector"}')
+
+export ELASTICSEARCH_APIKEY=$(echo $output | jq -r '.encoded')
+echo $ELASTICSEARCH_APIKEY
