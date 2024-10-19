0eccc4e985ed227353e35a2973ff95841cd93e7d
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-10-18 21:12:00 -0500

    ty

--- src/frontend/upload_source_map.sh
+++ src/frontend/upload_source_map.sh
@@ -1,12 +1,12 @@
 for i in static/js/*.js; do
   echo "${i}"
-  curl -iv --limit-rate 100K -X POST "$KIBANA_URL/api/apm/sourcemaps" \
+  curl -s -iv --limit-rate 400K -X POST "$KIBANA_URL/api/apm/sourcemaps" \
   -H 'Content-Type: multipart/form-data' \
   -H 'kbn-xsrf: true' \
   -u "${ELASTICSEARCH_USER}:${ELASTICSEARCH_PASSWORD}" \
   -F "service_name=${SERVICE_NAME}" \
   -F "service_version=${SERVICE_VERSION}" \
   -F "bundle_filepath=/${i}" \
-  -F "sourcemap=@${i}.map" || true
+  -F "sourcemap=@${i}.map" > /dev/null 2>&1
   sleep 5
 done
