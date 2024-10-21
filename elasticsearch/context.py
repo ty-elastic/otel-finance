3993426036034025e15ef290a3218de11a31f565
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-10-21 15:01:43 -0500

    ty

--- elasticsearch/context.py
+++ elasticsearch/context.py
@@ -71,20 +71,20 @@ def load_elser():
     
 
 def load_knowledge():
-
     for file in os.listdir(KNOWLEDGE_RESOURCES_PATH):
         if file.endswith(".json"):
             with open(os.path.join(KNOWLEDGE_RESOURCES_PATH, file), "rt", encoding='utf8') as f:
                 body = f.read()
                 filename = Path(file).stem
-
                 resp = requests.put(f"{os.environ['ELASTICSEARCH_URL']}/.kibana-observability-ai-assistant-kb-000001/_doc/${filename}?pipeline=.kibana-observability-ai-assistant-kb-ingest-pipeline",
                                      data=body, timeout=TIMEOUT,
                                      auth=(os.environ['ELASTICSEARCH_USER'], os.environ['ELASTICSEARCH_PASSWORD']),
                                      headers={"Content-Type": "application/json"})
-                print(resp.json())  
+                print(f"loading knowledge {filename}: {resp.json()}")  
 
 def load():
     load_elser()
     load_knowledge()
     load_indices()
+
+#load_knowledge()
