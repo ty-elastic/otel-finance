e85bb52c2467e57b30e01d81a56e8d648d479d23
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-10-28 21:30:21 -0500

    ty

--- resources/slo.py
+++ resources/slo.py
@@ -1,9 +1,36 @@
 import requests
 import os
+import json
 
 TIMEOUT = 10
 
 SLO_RESOURCES_PATH  = 'slo'
+
+def delete(id):
+    res = requests.delete(f"{os.environ['KIBANA_URL']}/api/observability/slos/{id}",
+                                    timeout=TIMEOUT,
+                                    auth=(os.environ['ELASTICSEARCH_USER'], os.environ['ELASTICSEARCH_PASSWORD']),
+                                    headers={"kbn-xsrf": "reporting", "Content-Type": "application/json"})
+    print(res)
+
+def clear():
+    slos = requests.get(f"{os.environ['KIBANA_URL']}/api/observability/slos",
+                                     timeout=TIMEOUT,
+                                     auth=(os.environ['ELASTICSEARCH_USER'], os.environ['ELASTICSEARCH_PASSWORD']),
+                                     headers={"kbn-xsrf": "reporting", "Content-Type": "application/json"})
+    for slo in slos.json()['results']:
+        delete(slo['id'])
+        
+def exists(name):
+    slos = requests.get(f"{os.environ['KIBANA_URL']}/api/observability/slos",
+                                     timeout=TIMEOUT,
+                                     auth=(os.environ['ELASTICSEARCH_USER'], os.environ['ELASTICSEARCH_PASSWORD']),
+                                     headers={"kbn-xsrf": "reporting", "Content-Type": "application/json"})
+    for slo in slos.json()['results']:
+        if slo['name'] == name:
+            return slo['id']
+    return None
+
 
 def load():
 
@@ -11,9 +38,15 @@ def load():
         if file.endswith(".json"):
             with open(os.path.join(SLO_RESOURCES_PATH, file), "rt", encoding='utf8') as f:
                 body = f.read()
+                
+                id = exists(json.loads(body)['name'])
+                if id is not None:
+                    print('found, deleting old slo')
+                    delete(id)
 
                 resp = requests.post(f"{os.environ['KIBANA_URL']}/api/observability/slos",
                                      data=body, timeout=TIMEOUT,
                                      auth=(os.environ['ELASTICSEARCH_USER'], os.environ['ELASTICSEARCH_PASSWORD']),
                                      headers={"kbn-xsrf": "reporting", "Content-Type": "application/json"})
                 print(resp.json())  
+                
