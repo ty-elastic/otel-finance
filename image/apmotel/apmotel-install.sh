75f0c0e4fd988cfe68c6c9ee59c49ee84c8d6f39
Tyrone Bekiares <tyrone.bekiares@elastic.co>
2024-12-20 20:20:20 -0600

    changes

---
+++ image/apmotel/apmotel-install.sh
@@ -0,0 +1,22 @@
+####################################################################### CODE
+
+export $(cat /root/.env | xargs)
+
+WORKSPACE_DIR=/workspace
+GIT_BRANCH=main
+GIT_URL=https://github.com/ty-elastic/otel-finance.git
+
+mkdir -p $WORKSPACE_DIR
+cd $WORKSPACE_DIR
+git config --global init.defaultBranch $GIT_BRANCH
+git init
+git remote add origin $GIT_URL
+git pull
+git checkout $GIT_BRANCH -f
+git branch --set-upstream-to origin/$GIT_BRANCH
+
+mkdir -p $WORKSPACE_DIR/logs
+
+echo "cd $WORKSPACE_DIR" >> /root/.bashrc
+
+docker compose build
